b4475cca332a53587b9b6f1012a07966182148fc
HARSH BATRA <harshbat22@gmail.com>
2024-01-31 17:25:08 +0530

    updated app.js to resolve cors error

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,16 @@ const connectDB = async () => {
 //middlewares
 dotenv.config();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://bloggify.vercel.app");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(cors());
 app.use(cookieParser());
